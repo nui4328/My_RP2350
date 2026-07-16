c38242398184ea49e2240b8dc521b883cd419942
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2026-07-16 19:57:52 +0700

    Add files via upload

---
+++ PiperPico2/example/piper_OPT1234_055/g2.ino
@@ -0,0 +1,114 @@
+void mission_2() 
+  {
+    arm_updown(0);
+    setrobot_bw(2);   //----->>ถอยหลังเซต; 
+
+    move(40,  40, 1.5, 30, 10, 2);
+    turnGyro(70, -90, true); 
+    setrobot_bw(2);   //----->>ถอยหลังเซต; 
+    
+    move(40,  40, 1.5, 30, 10, 2);
+    turnGyro(70, -180, true); 
+    setrobot_bw(2);   //----->>ถอยหลังเซต; 
+
+    arm_ready();
+    go_mission_on_floor();  // เข้าคีบที่พื้น
+    
+    armLR_to_can();   //เอากระป๋องจากมือทั้ง 2 ข้าง ไปเก็บที่ช่องเก็บ 
+    
+    
+    move(-40,  -40, 1.5, 30, 0, 2);
+    setrobot_bw(2);   //----->>ถอยหลังเซต; 
+    turnGyro(70, -90, true); 
+
+    arm_ready();
+    go_mission_on_floor();  // เข้าคีบที่พื้น
+
+    arm_updown(7); 
+    setrobot_fw(2);   //----->>ถอยหลังเซต; 
+    turnGyro(70, 180, true); 
+    setrobot_bw(2);   //----->>ถอยหลังเซต; 
+
+    arm_updown(13); 
+    go_to_mission_on_stand('L',  70, 9);   // ตัวแปรตัวสุดท้ายคือ ความสูงของแขน  เดินวางสีแดง
+    move(-40,  -40, 1.5, 20, 10, 17);
+    armR_to_armL();  //------->> เปลี่ยนกระป๋องจากมือ ขวา ไปมือ ซ้าย
+
+    can_to_armR();    //เอากระป๋องจากที่ช่องขวา ไปให้มือขวา
+     arm_updown(13); 
+     go_to_mission_on_stand('R',  60, 11);   // ตัวแปรตัวสุดท้ายคือ ความสูงของแขน
+
+
+
+
+
+     sw();
+    
+   arm_bridge();
+
+    move(40,  40, 1.5, 60, 0, 2);
+    setrobot_fw(2);   //----->>ถอยหลังเซต; 
+    
+     turnGyro(70, -90, true); 
+    
+     move(-40,  -40, 1.5, 30, 0, 2);
+     setrobot_bw(2);   //----->>ถอยหลังเซต; 
+     turnGyro(70, 0, true); 
+     
+    move(-40,  -40, 1.5, 30, 0, 2);
+    setrobot_bw(2);   //----->>ถอยหลังเซต; 
+
+    turnGyro(70, -90, true); 
+    
+
+     move(60, 60, 1.5, 48, 0, 2);
+    setrobot_fw(2);   //----->>ถอยหลังเซต; 
+    turnGyro(70, -180, true); 
+    setrobot_bw(2);   //----->>ถอยหลังเซต; 
+
+    move_chopsticks(40,  40, 1.5, 60, 0, 2);
+
+    move(40,  40, 1.5, 30, 20, 5);
+    setrobot_fw(2);   //----->>ถอยหลังเซต; 
+    turnGyro(70, 90, true); 
+    arm_updown(0);
+
+
+    move(40,  40, 1.5, 25, 10, 2);
+    turnGyro(70, 0, true); 
+    setrobot_bw(2);   //----->>ถอยหลังเซต;
+    arm_ready();
+    delay(300);
+    turnGyro(70, 90, true);
+
+    go_mission_on_stand(); //เข้าคีบบนแท่น 5 เซนติเมตร
+
+     move(-40,  -40, 1.5, 37, 10, 2);
+     turnGyro(70, 0, true); 
+     setrobot_bw(2);   //----->>ถอยหลังเซต; 
+    
+    move(40,  40, 1.5, 30, 10, 2);
+    turnGyro(70, 90, true); 
+
+    move(40,  40, 1.5, 30, 10, 2);
+    turnGyro(70, 0, true); 
+     move(40,  40, 1.5, 30, 10, 2);
+    setrobot_fw(2);   //----->>ถอยหลังเซต; 
+    turnGyro(70, 90, true);
+    arm_updown(19); 
+    go_to_mission_on_stand('L',  70, 17);   // ตัวแปรตัวสุดท้ายคือ ความสูงของแขน  เดินวางสีแดง
+    move(-40,  -40, 1.5, 20, 10, 17);
+    armR_to_armL();  //------->> เปลี่ยนกระป๋องจากมือ ขวา ไปมือ ซ้าย
+
+    can_to_armR();    //เอากระป๋องจากที่ช่องขวา ไปให้มือขวา
+     arm_updown(19); 
+     go_to_mission_on_stand('R',  60, 17);   // ตัวแปรตัวสุดท้ายคือ ความสูงของแขน
+    
+
+
+
+
+
+
+
+  }
